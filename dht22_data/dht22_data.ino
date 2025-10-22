0d4053bf6b9542bea19dfb5da08b6adf5d909be2
Joona Pekkala <joona.pekkala13@gmail.com>
2025-10-22 18:04:04 +0300

    Initial commit

---
+++ dht22_data/dht22_data.ino
@@ -0,0 +1,27 @@
+#include "DHT.h"              // Include library
+
+#define DHTPIN 14             // Define pin
+#define DHTTYPE DHT22         // Define sensor
+
+DHT dht(DHTPIN, DHTTYPE);     // Create sensor object
+
+void setup() {
+  Serial.begin(9600);         // Open serial
+  dht.begin();                // Start the sensor
+}
+
+void loop() {
+  float temp = dht.readTemperature(); // Read temperature
+  float hum = dht.readHumidity();     // Read humidity
+
+  if (isnan(temp) || isnan(hum)) {
+    Serial.println("Error reading DHT22!");
+    return;
+  }
+
+  Serial.print(temp);
+  Serial.print(","); 
+  Serial.println(hum);
+
+  delay(1000);                        // Wait 2 seconds
+}
---
+++ python_app/main.py
@@ -0,0 +1,43 @@
+# Needed libraries
+import os
+import serial
+import time
+import csv
+from datetime import datetime
+
+
+def read_from_serial(port = "COM3", baudrate = 9600, timeout = 2):
+    try:
+        ser = serial.Serial(port, baudrate, timeout = timeout)
+        line = ser.readline().decode("utf-8").strip()
+        ser.close()
+        if line:
+            temp, hum = map(float, line.split(","))
+            return temp, hum
+    except Exception as e:
+        print(f"Serial read error: {e}")
+    return None, None
+
+# Write the data to csv file and print to terminal
+def write_to_csv(filename = "data.csv", duration = 10, interval = 1):     # Duration for total logging in seconds and interval between measurement in seconds
+    file_exists = os.path.exists(filename)                                # Check if CSV already exists
+    with open(filename, "a", newline = "") as file:
+        writer = csv.writer(file)
+        if not file_exists:
+            writer.writerow(["timestamp", "temp_C", "hum_%"])           # Create the first line in csv if the file has not been created yet
+        print("Timestamp: Temp (C), Hum (%)")                           # Terminal print
+        start_time = time.time()
+        try:
+            while time.time() - start_time < duration:                  # While loop to measure data for a wanted time
+                timestamp = datetime.now().isoformat()                  # Timestamp for data
+                temp, hum = read_from_serial()
+                writer.writerow([timestamp, temp, hum])
+                print(f"{timestamp}: Temperature: {temp} C, Humidity: {hum} %")     # Terminal print
+                time.sleep(interval)
+        except KeyboardInterrupt:                                                   # Handle user interruption
+            print("\nData logging stopped by user.")
+
+        print(f"\nData logging complete. Data can be found at {filename}.")
+
+if __name__ == "__main__":
+    write_to_csv()
